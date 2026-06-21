4c7f824e10a574a4a8571bd5065af80609a13820
Kunal <klutzykunal@gmail.com>
2026-06-21 18:12:51 +0530

    Fix collectstatic with --clear flag

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 python manage.py migrate
